0eae1f83117476cdeb0a077bb7198fe3d373ba74
Anthony Liatsis <anthony.liatsis@gmail.com>
2015-09-12 15:52:41 -0700

    import

--- platform/android/com/appsflyer/cordova/plugin/AppsFlyerPlugin.java
+++ platform/android/com/appsflyer/cordova/plugin/AppsFlyerPlugin.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
